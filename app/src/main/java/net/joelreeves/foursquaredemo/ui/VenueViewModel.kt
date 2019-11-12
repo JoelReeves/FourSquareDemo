8def2e59832114d745c645a713b6014ae92f8998
Joel Reeves <jmrmb80@gmail.com>
2019-11-12 13:00:51 -0500

    viewmodel class used to load and send the list of venues to the UI
    Change-Id: Ic261d211a70bb46ce7377e22d2a851d03f8736bb

---
+++ app/src/main/java/net/joelreeves/foursquaredemo/ui/VenueViewModel.kt
@@ -0,0 +1,51 @@
+package net.joelreeves.foursquaredemo.ui
+
+import androidx.lifecycle.*
+import io.reactivex.schedulers.Schedulers
+import net.joelreeves.foursquaredemo.data.models.venuemodel.Venue
+import net.joelreeves.foursquaredemo.data.services.VenueRepository
+import javax.inject.Inject
+
+class VenueViewModel @Inject constructor(private val repository: VenueRepository) : ViewModel() {
+
+    companion object {
+        private val TAG = VenueViewModel::class.java.simpleName
+    }
+
+    private val latLng = MutableLiveData<String>()
+    private val city = MutableLiveData<String>()
+
+    val venuesNearMe: LiveData<List<Venue>> =
+        Transformations.switchMap(latLng) {
+            LiveDataReactiveStreams.fromPublisher(
+                repository.findVenuesNearLocation(it).toObservable()
+                    .flatMapIterable { venues -> venues }
+                    .flatMap { venue -> repository.findVenueById(venue).toObservable() }
+                    .toList()
+                    .onErrorReturn { emptyList() }
+                    .subscribeOn(Schedulers.io())
+                    .toFlowable()
+            )
+        }
+
+    val venuesNearCity: LiveData<List<Venue>> =
+        Transformations.switchMap(city) {
+            LiveDataReactiveStreams.fromPublisher(
+                repository.findVenuesNearCity(it).toObservable()
+                    .flatMapIterable { venues -> venues }
+                    .flatMap { venue -> repository.findVenueById(venue).toObservable() }
+                    .toList()
+                    .onErrorReturn { emptyList() }
+                    .subscribeOn(Schedulers.io())
+                    .toFlowable()
+            )
+        }
+
+    fun loadVenuesNearMe(latLngString: String) {
+        latLng.value = latLngString
+    }
+
+    fun loadVenuesNearCity(cityString: String) {
+        city.value = cityString
+    }
+}
