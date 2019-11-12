80df535f9d4420ebd1335c6bedaf1873804bc983
Joel Reeves <jmrmb80@gmail.com>
2019-11-12 13:07:50 -0500

    recyclerview adapter for displaying list of venues
    Change-Id: I8b796f6de9bee5426b9dfffee60efe3aed752be7

--- app/src/main/java/net/joelreeves/foursquaredemo/ui/VenuesAdapter.kt
+++ app/src/main/java/net/joelreeves/foursquaredemo/ui/VenuesAdapter.kt
@@ -1,26 +1,55 @@
 package net.joelreeves.foursquaredemo.ui
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import net.joelreeves.foursquaredemo.R
+import net.joelreeves.foursquaredemo.data.models.venuemodel.Venue
+import net.joelreeves.foursquaredemo.utils.VenueFormatter
+import net.joelreeves.foursquaredemo.utils.ImageLoader
 
-class VenuesAdapter() : RecyclerView.Adapter<VenuesAdapter.VenueHolder>() {
+class VenuesAdapter(
+    private val venues: List<Venue>,
+    private val imageLoader: ImageLoader,
+    private val listener: VenueListener) :
+    RecyclerView.Adapter<VenuesAdapter.VenueHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VenueHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_venue, parent, false)
+        return VenueHolder(view, listener)
     }
 
     override fun onBindViewHolder(holder: VenueHolder, position: Int) {
+        holder.bindVenue(venues[position])
     }
 
-    override fun getItemCount(): Int {
-    }
+    override fun getItemCount(): Int = venues.size
 
+    inner class VenueHolder(view: View, private val listener: VenueListener) :
+        RecyclerView.ViewHolder(view) {
+        private val venueImage: ImageView = view.findViewById(R.id.venue_image)
+        private val venueName: TextView = view.findViewById(R.id.venue_name)
+        private val venueDistance: TextView = view.findViewById(R.id.venue_distance)
+        private val venueCity: TextView = view.findViewById(R.id.venue_city)
+        private val venueRating: TextView = view.findViewById(R.id.venue_rating)
 
-    inner class VenueHolder(view: View, private val listener: VenueListener) : RecyclerView.ViewHolder(view) {
+        private val formatter by lazy { VenueFormatter }
 
+        fun bindVenue(venue: Venue) {
+            itemView.setOnClickListener {listener.onVenueClick(venue)}
+            imageLoader.loadImage(formatter.formatVenueImageUrl(venue), venueImage)
+            venueName.text = venue.name
+            venueDistance.text = formatter.formatMiles(itemView.resources, venue.distance)
+            venueCity.text = venue.location.city
+            venueRating.text = venue.rating.toString()
+            venueRating.setTextColor(formatter.formatRatingColor(venue.ratingColor))
+        }
     }
 
     interface VenueListener {
-        fun onVenueClick()
+        fun onVenueClick(venue: Venue)
     }
 }
