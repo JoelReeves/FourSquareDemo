da756a6ef67430a86ce2b558aa7253b7c435baff
Joel Reeves <jmrmb80@gmail.com>
2019-12-03 20:31:54 -0500

    wip viewmodel tests
    Change-Id: Ibacf11a8edcdcc411419b4feb132f351cfe500dc

---
+++ app/src/test/java/net/joelreeves/foursquaredemo/ui/VenueViewModelTests.kt
@@ -0,0 +1,10 @@
+package net.joelreeves.foursquaredemo.ui
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+
+class VenueViewModelTests {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+}
